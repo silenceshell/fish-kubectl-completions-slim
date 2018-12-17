7fd48883084313c3f24345d81e79eab83b3f283c
silenceshell <me@ieevee.com>
2018-12-17 19:48:50 +0800

    v1

---
+++ README.md
@@ -0,0 +1,24 @@
+# kubectl completion for fish shell slim
+
+## Why
+
+This project is inspired by [fish-kubectl-completions](https://github.com/evanlucas/fish-kubectl-completions.git).
+
+Why not use this project directly? It works **VERY** slowly on my workstation, normally I have to wait up to 10 to 20 seconds for the output.
+
+So I decided to write a slim one, and it works quite well.
+
+## Install
+
+```fish
+$ mkdir -p ~/.config/fish/completions
+$ cd ~/.config/fish
+$ git clone https://github.com/silenceshell/fish-kubectl-completions-slim.git
+$ cp ./fish-kubectl-completions-slim/kubectl.fish completions/
+```
+
+Relogin to fish and enjoy!
+
+## Author
+
+silenceshell
---
+++ kubectl.fish
@@ -0,0 +1,54 @@
+set __kubectl_resources          \
+  all                            \
+  clusterrolebindings            \
+  clusterroles                   \
+  clusters                       \
+  configmaps cm        \
+  cronjobs                       \
+  daemonsets ds                  \
+  deployments  \
+  endpoints ep                   \
+  events ev                      \
+  horizontalpodautoscalers hpa   \
+  ingresses ing          \
+  jobs                           \
+  limitranges limits             \
+  namespaces ns        \
+  networkpolicies netpol         \
+  nodes no                  \
+  persistentvolumeclaims pvc     \
+  persistentvolumes pv           \
+  pods                     \
+  replicasets rs                 \
+  replicationcontrollers rc      \
+  resourcequotas quota           \
+  rolebindings                   \
+  roles                          \
+  secrets                  \
+  serviceaccounts sa             \
+  services svc           \
+  statefulsets               \
+  storageclass storageclasses sc
+
+function __fish_print_resource -d 'Print a list of resources' -a resource
+  kubectl get "$resource" -o name \
+    | string replace -r '(.*)/' ''
+end
+
+set __fish_kubectl_subresource_commands get describe delete edit
+
+for subcmd in $__fish_kubectl_subresource_commands
+  for r in $__kubectl_resources
+      complete -f -c kubectl -n "__fish_seen_subcommand_from $subcmd; and __fish_seen_subcommand_from $r" -a '(__fish_print_resource $r)' -d 'Resource'
+  end
+
+  complete -c kubectl -f -n "__fish_seen_subcommand_from $subcmd; and not __fish_seen_subcommand_from (__fish_print_resource_types)" -a '(__fish_print_resource_types)' -d 'Resource'
+end
+
+complete -c kubectl -A -f -n '__fish_seen_subcommand_from exec' -a '(__fish_print_resource pods)' -d "Pod"
+
+function __fish_print_resource_types
+  for r in $__kubectl_resources
+    echo $r
+  end
+end
